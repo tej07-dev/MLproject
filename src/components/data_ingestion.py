17107c3909e01ce1ef305cf774db378825e18bbc
dev07 <tejal.mulay1207@gmail.com>
2025-08-17 15:26:49 +0530

    final commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -42,11 +42,11 @@ class DataIngestion:
             
 
 if __name__=="__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+     obj = DataIngestion()
+     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+     data_transformation = DataTransformation()
+     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
 
-    modeltrainer = ModelTrainer()
-    modeltrainer.initiate_model_trainer(train_arr, test_arr)
+     modeltrainer = ModelTrainer()
+     modeltrainer.initiate_model_trainer(train_arr, test_arr)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -26,9 +26,9 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         try:
-            numerical_columns= ["writing score","reading score"]
-            categorical_columns=["gender","race/ethnicity","parental level of education",
-                                 "lunch","test preparation course"]
+            numerical_columns= ["writing_score","reading_score"]
+            categorical_columns=["gender","race_ethnicity","parental_level_of_education",
+                                 "lunch","test_preparation_course"]
             
             num_pipeline=Pipeline(steps=[
                 ("imputer",SimpleImputer(strategy="median")),
@@ -64,8 +64,8 @@ class DataTransformation:
             logging.info("obtaining preprocessing object")
             preprocessor_obj=self.get_data_transformer_object()
 
-            target_column_name="math score"
-            numerical_columns=["writing score","reading score"]
+            target_column_name="math_score"
+            numerical_columns=["writing_score","reading_score"]
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
 
